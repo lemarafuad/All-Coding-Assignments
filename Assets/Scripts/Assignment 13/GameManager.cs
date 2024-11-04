46f808104593be5a65991e438d3226404c94c8b3
lemarafuad <lemarfuad7@gmail.com>
2024-11-04 21:50:25 +0200

    push assignment 13 in repo

---
+++ Assets/Scripts/Assignment 13/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player player1=new Player();
+        Player player2=new Player();
+        
+        player1.InitializePlayer("LM",21);
+        player2.InitializePlayer("RT",60);
+        
+        player1.Heal(20);
+        player2.Heal(5);
+
+        player1.Heal(true);
+        player2.Heal(true);
+        
+        Player.ShowPlayerCount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

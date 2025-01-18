d4c69a53f6653c77ab36fda6860285b1f23b91a6
lemarafuad <lemarafuad7@gmail.com>
2025-01-18 21:09:01 +0200

    submit Assignment37

---
+++ Assets/Scripts/Assignment37/rigidbody-controller/RigidBodyCharacterController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyCharacterController : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Vector3 input;
+    bool jump = false;
+    float battahSpeed = 4f;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
+        rigidbody.mass = 1;
+
+    }
+
+    void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        input = input.normalized * battahSpeed;
+        input.y = rigidbody.velocity.y;
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (jump)
+        {
+            rigidbody.drag = 0.5f;
+            rigidbody.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            jump = false;
+        }
+        else
+        {
+            rigidbody.drag = 0;
+            rigidbody.velocity = input;
+        }
+    }
+}
